1bfcec62f917f2f8f88af3a5e7871b590b2d838f
luozheng <qssq666@foxmail.com>
2018-08-09 10:16:25 +0800

    10分钟写完toast/ 获取application

--- app/src/main/cpp/mylib.cpp
+++ app/src/main/cpp/mylib.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include <android/log.h>
+
 #define LOG_TAG "TEST_JNI"
 
 #define DEBUG
@@ -15,15 +16,15 @@
 #define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__))
 #else  //如果 不是 android平台 直接替换 。
 #define LOGD(fmt, ...) printf(fmt"\n", ##__VA_ARGS__)
-		#define LOGI(fmt, ...) printf(fmt"\n", ##__VA_ARGS__)
-		#define LOGW(fmt, ...) printf(fmt"\n", ##__VA_ARGS__)
-		#define LOGE(fmt, ...) printf(fmt"\n", ##__VA_ARGS__)
+#define LOGI(fmt, ...) printf(fmt"\n", ##__VA_ARGS__)
+#define LOGW(fmt, ...) printf(fmt"\n", ##__VA_ARGS__)
+#define LOGE(fmt, ...) printf(fmt"\n", ##__VA_ARGS__)
 #endif
 #else
 #define LOGD(...)
-	#define LOGI(...)
-	#define LOGW(...)
-	#define LOGE(...)
+#define LOGI(...)
+#define LOGW(...)
+#define LOGE(...)
 
 #endif
 
@@ -72,11 +73,12 @@ extern "C" JNIEXPORT jint JNICALL
 Java_cn_qssq666_testjni_MainActivity_calcAdd(JNIEnv *env, jobject, jint value, jint value1) {
 
 
-
     int reuslt = strlen("555");
     if (reuslt > 10) {
         exit(0);
+
     }
+
 
     return value + value1;
 
@@ -109,23 +111,43 @@ extern "C" JNIEXPORT jint JNICALL
 Java_cn_qssq666_testjni_MainActivity_testCRepleaceValue(JNIEnv *env, jobject instance, jint i) {
 
     // int 类型的指针 *p指针指向int变量i的内存地址。
-    int *p = &i;//取地址
+    env->MonitorEnter(instance);
+    int *p = &i;//定义指针 指向这个地址
 
     printf("cvalue %d", i);
     *p = 150;
-    printf("cvalue repleace address value %d", i);
+    int **px = &p;
+    **px = 3330;
+    LOGW("cvalue repleace  value %d %d %d", *p, i, **px);//取值 可是java那边 不会改变
+    LOGW("%#x %#x %#x", *p, i, **px);//print address
+
+
     // 修改int指针指向的内存地址的值。
+
+    env->MonitorExit(instance);
 
     return *p;
 
 
+    /*
+     * int *p ：一级指针，表示p所指向的地址里面存放的是一个int类型的值 
+int **p ：二级指针，表示p所指向的地址里面存放的是一个指向int类型的指针（即p指向的地址里面存放的是一个指向int的一级指针） 
+例如： 
+int i=10; //定义了一个整型变量 
+int *p=&i; //定义了一个指针指向这个变量 
+int **p1=&p; //定义了一个二级指针指向p指针 
+那么取出10的值方式为： 
+printf(“i=[%d]\n”,*p); 
+printf(“i=[%d]\n”,**p1);
+     */
+
 
 }
 
-void exit_fn1(void)
-{
+void exit_fn1(void) {
     printf("Exit function #1 called\n");
 }
+
 extern "C" JNIEXPORT void JNICALL
 Java_cn_qssq666_testjni_MainActivity_exist1(JNIEnv *env, jobject instance) {
 
@@ -133,15 +155,19 @@ Java_cn_qssq666_testjni_MainActivity_exist1(JNIEnv *env, jobject instance) {
 
 }extern "C" JNIEXPORT void JNICALL
 Java_cn_qssq666_testjni_MainActivity_exit2(JNIEnv *env, jobject instance) {
-
+    LOGE("执行 exit start");
 
     _Exit(0);
 
+
+
+    LOGE("执行 exit end");
 }
 extern "C" JNIEXPORT void JNICALL
 Java_cn_qssq666_testjni_MainActivity_exit3(JNIEnv *env, jobject instance) {
-
-    atexit(exit_fn1);
+    LOGE("执行 exit start");
+    atexit(exit_fn1);//毫无效果
+    LOGE("执行 exit end");
 }
 
 extern "C" JNIEXPORT void JNICALL
@@ -150,4 +176,182 @@ Java_cn_qssq666_testjni_MainActivity_exit1(JNIEnv *env, jobject instance) {
     LOGE("执行 exit start");
     abort();
     LOGE("执行 exit end");
+}
+extern "C" JNIEXPORT jint JNICALL
+Java_cn_qssq666_testjni_MainActivity_execute_1c(JNIEnv *env, jobject instance) {
+
+    int value = strcmp("x", "y");
+    return value;
+
+
+}
+extern "C" JNIEXPORT void JNICALL
+Java_cn_qssq666_testjni_MainActivity_testThrowError(JNIEnv *env, jclass type) {
+
+
+    env->FatalError("test_throw_error");
+
+}
+extern "C" JNIEXPORT jint JNICALL
+Java_cn_qssq666_testjni_MainActivity_getJVMVersion(JNIEnv *env, jclass type) {
+
+    return env->GetVersion();
+
+
+}
+extern "C" JNIEXPORT jboolean JNICALL
+Java_cn_qssq666_testjni_MainActivity_checkFileExist(JNIEnv *env, jobject instance, jstring path_) {
+    jboolean value = JNI_TRUE;
+    const char *path = env->GetStringUTFChars(path_, &value);
+    LOGW("PATH %s", path);
+    FILE *fp = fopen(path, "r");
+
+    bool exist = fp != NULL;
+    if (exist) {
+        fclose(fp);
+    }
+
+
+//    env->
+    env->ReleaseStringUTFChars(path_, path);
+
+    return exist;
+}extern "C" JNIEXPORT void JNICALL
+Java_cn_qssq666_testjni_MainActivity_moni_1exit(JNIEnv *env, jobject instance) {
+
+    env->MonitorEnter(instance);
+    env->MonitorExit(instance);
+
+
+}extern "C" JNIEXPORT void JNICALL
+Java_cn_qssq666_testjni_MainActivity_showToast(JNIEnv *env, jobject instance) {
+
+
+    //currentApplication
+    jclass jclassActivityThead = env->FindClass("android/app/ActivityThread");
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到");
+        env->ExceptionClear();
+        return;
+
+    }
+
+    //currentApplication () {
+
+    jmethodID currentApplicationId = env->GetStaticMethodID(jclassActivityThead,
+            "currentApplication", "()Landroid/app/Application;");
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到application method");
+        env->ExceptionClear();
+        return;
+
+    }
+
+    jobject jobjectApplication = env->CallStaticObjectMethod(jclassActivityThead,
+            currentApplicationId);
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到application");
+        env->ExceptionClear();
+        return;
+
+    }
+
+    jclass toastClass = env->FindClass("android/widget/Toast");
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到Toast");
+        env->ExceptionClear();
+        return;
+
+    }
+
+
+    jmethodID makeTextMethodId = env->GetStaticMethodID(toastClass, "makeText",
+            "(Landroid/content/Context;Ljava/lang/CharSequence;I)Landroid/widget/Toast;");
+
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到Toast Method ");
+        env->ExceptionClear();
+        return;
+
+    }
+
+    jstring str = env->NewStringUTF("你被我拦截!");
+
+    jint duration = 0;
+    jobject toastObj = env->CallStaticObjectMethod(toastClass, makeTextMethodId, jobjectApplication,
+            str, duration);
+
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到Toast Method ");
+        env->ExceptionClear();
+        return;
+
+    }
+
+    jmethodID toastShowJmi = env->GetMethodID(toastClass, "show", "()V");
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到Toast show methodid ");
+        env->ExceptionClear();
+        return;
+
+    }
+
+    if (jobjectApplication == NULL) {
+        LOGE("没有application");
+    }
+    env->CallVoidMethod(toastObj, toastShowJmi);
+
+
+
+
+    //  public static Toast makeText(Context context, CharSequence text, @Duration int duration) {
+
+
+
+
+
+
+    LOGW("application address:%p", *jobjectApplication);
+
+
+//https://blog.csdn.net/yuzhou_zang/article/details/78410632
+
+}
+extern "C" JNIEXPORT jstring JNICALL
+Java_cn_qssq666_testjni_MainActivity_getApplicationPackage(JNIEnv *env, jobject instance) {
+
+
+
+    //currentApplication
+    jclass jclassActivityThead = env->FindClass("android/app/ActivityThread");
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到");
+        env->ExceptionClear();
+        return env->NewStringUTF("没有找到  ActivityThread");
+
+    }
+
+    //currentApplication () {
+
+    jmethodID currentApplicationId = env->GetStaticMethodID(jclassActivityThead,
+            "currentPackageName", "()Ljava/lang/String;");
+    if (env->ExceptionCheck()) {
+        LOGE("没有找到 currentPackageName method");
+        env->ExceptionClear();
+        return env->NewStringUTF("没有找到 currentPackageName method");
+
+    }
+
+    jstring packageName = static_cast<jstring>(env->CallStaticObjectMethod(jclassActivityThead,
+                currentApplicationId));
+    if (env->ExceptionCheck()) {
+        LOGE("call fail");
+        env->ExceptionClear();
+        return env->NewStringUTF("currentPackageName call fail");
+
+    }
+
+
+
+    return packageName;
 }
--- app/src/main/java/cn/qssq666/testjni/MainActivity.java
+++ app/src/main/java/cn/qssq666/testjni/MainActivity.java
@@ -2,7 +2,9 @@ package cn.qssq666.testjni;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,6 +37,14 @@ public class MainActivity extends Activity {
 
                 String s = info.toString();
                 Toast.makeText(MainActivity.this, "" + getMessage() + "," + getMessage1() + message + "," + s, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        findViewById(R.id.btn_throwerror).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.testThrowError(); //libart.so (_ZN3art8CheckJNI10FatalErrorEP7_JNIEnvPKc+648)
 
             }
         });
@@ -71,7 +81,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 MainActivity.this.exist1();
-                Toast.makeText(MainActivity.this, "xxxx",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "xxxx", Toast.LENGTH_LONG).show();
 
             }
         });
@@ -80,7 +90,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 MainActivity.this.exit1();
-                Toast.makeText(MainActivity.this, "test2",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "test2", Toast.LENGTH_LONG).show();
 
             }
         });
@@ -88,7 +98,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 MainActivity.this.exit2();
-                Toast.makeText(MainActivity.this, "test2",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "test2", Toast.LENGTH_LONG).show();
 
             }
         });
@@ -97,14 +107,14 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 MainActivity.this.exit3();
-                Toast.makeText(MainActivity.this, "test2",Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "test3", Toast.LENGTH_LONG).show();
 
             }
         });
         findViewById(R.id.btn_c_repleace_value).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int value = 30;
+
                 testCRepleaceValue(value);
                 Toast.makeText(MainActivity.this, "传递后结果:" + value, Toast.LENGTH_SHORT).show();
             }
@@ -112,7 +122,66 @@ public class MainActivity extends Activity {
         });
 
 
+        findViewById(R.id.btn_execute_c).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int value = 30;
+                execute_c();
+                Toast.makeText(MainActivity.this, "exec over :", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        findViewById(R.id.btn_jvm_ver).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int jvmVersion = getJVMVersion();
+                Toast.makeText(MainActivity.this, "jvm version:" + jvmVersion, Toast.LENGTH_SHORT).show();
+            }
+        });
+        final EditText evPath = (EditText) findViewById(R.id.ev_path);
+
+        findViewById(R.id.btn_check_file).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String path = evPath.getText().toString();
+                if(TextUtils.isEmpty(path)){
+                    path="/data/local/tmp/frida-server";
+                }
+                boolean exist = checkFileExist(path);
+                Toast.makeText(MainActivity.this, "file is exist?" + exist, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        findViewById(R.id.btn_test_moni_exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        moni_exit();
+            }
+        });
+
+
+        findViewById(R.id.btn_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast();
+            }
+        });
+
+        findViewById(R.id.btn_package).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"apppackage:"+getApplicationPackage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+    int value = 30;
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
@@ -131,9 +200,27 @@ public class MainActivity extends Activity {
     public native int exit();
 
     public native void exist1();
+
     public native void exit1();
+
     public native void exit2();
+
     public native void exit3();
 
     public native int testCRepleaceValue(int i);
+
+    public native static void testThrowError();
+
+    public native static int getJVMVersion();
+
+
+    public native int execute_c();
+    public native void moni_exit();
+
+    public native boolean checkFileExist(String path);
+
+
+    public native  void showToast();
+    public native  String getApplicationPackage();
+
 }
