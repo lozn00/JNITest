2744f45d5363a431201cbd8a4ccdade973b05e82
luozheng <qssq666@foxmail.com>
2018-10-25 16:53:36 +0800

    存档

---
+++ app/src/main/java/cn/qssq666/testjni/AppContext.java
@@ -0,0 +1,52 @@
+package cn.qssq666.testjni;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.tencent.bugly.crashreport.CrashReport;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Created by qssq on 2018/9/3 qssq666@foxmail.com
+ */
+public class AppContext extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext(), "18fc43a418", false);
+
+        CrashReport.testJavaCrash();
+    }
+
+    /**
+     * 获取进程号对应的进程名
+     *
+     * @param pid 进程号
+     * @return 进程名
+     */
+    private static String getProcessName(int pid) {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+            String processName = reader.readLine();
+            if (!TextUtils.isEmpty(processName)) {
+                processName = processName.trim();
+            }
+            return processName;
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
